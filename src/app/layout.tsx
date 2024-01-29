76c8560e3f50d2779da8ab5f6224b4ba6ffd3011
Florian Esslinger <32069282+florianess@users.noreply.github.com>
2024-01-29 22:21:40 +0000

    feat: init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import { Providers } from "./providers"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Baby Rank",
+  description: "Baby Rank",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <div className="border-b">
+            <div className="h-16 container flex justify-between items-center">
+              <h2>Baby Rank</h2>
+              <div className="flex gap-4 items-center">
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+                <SignedOut>
+                  <SignInButton />
+                </SignedOut>
+              </div>
+            </div>
+          </div>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
